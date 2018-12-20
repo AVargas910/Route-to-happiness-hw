fb431378b51963572ec76385423a19225ed788f3
Alvaro Vargas <vargasa910@gmail.com>
2018-12-20 18:54:46 -0500

    Routes to happiness homework

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './CSS/index.css';
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Happy from './components/Happy';
 import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
 
-// your code goes here
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route exact path='/' component={Home} />
+        <Route path='/happy' component={Happy} />
+        <Route path='/sleepy' component={Sleepy} />
+        <Route path='/guilty' component={Guilty} />
+      </div>
+    </Router>
+  )
+}
+
 class Home extends Component{
   render(){
     return(
-      // your code goes here
+      <Router>
+        <div id='menuBar'>
+          <Link to="/">Home</Link>
+          <Link to="/happy">Happy</Link>
+          <Link to="/sleepy">Sleepy</Link>
+          <Link to="/guilty">Guilty</Link>
+        </div>
+      </Router>
     )
   }
 }
